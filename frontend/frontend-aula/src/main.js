20ada156969c90d0f35997a7db832a6e7445888b
Marcos Aurélio <39933244+marcos-mass13@users.noreply.github.com>
2026-06-24 17:18:11 -0300

    testec

--- frontend/frontend-aula/src/main.js
+++ frontend/frontend-aula/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Bootstrap from 'bootstrap' 
+// import Bootstrap from 'bootstrap' 
 
 /* Bootstrap */
 import 'bootstrap/dist/css/bootstrap.min.css' 
+import 'bootstrap' 
 
 
 const app = createApp(App) 
