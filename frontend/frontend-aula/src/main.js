d6e02566d63038d275c1d74c9ab0e3c752cfc5b6
Marcos Aurélio <39933244+marcos-mass13@users.noreply.github.com>
2026-06-24 17:05:23 -0300

    att boot

--- frontend/frontend-aula/src/main.js
+++ frontend/frontend-aula/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Bootstrap from 'bootstrap' 
+//  Correto: Importa como um módulo nomeado
+import * as Bootstrap from 'bootstrap';
 
 /* Bootstrap */
 import 'bootstrap/dist/css/bootstrap.min.css' 
