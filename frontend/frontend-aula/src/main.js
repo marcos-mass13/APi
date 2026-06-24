0ba432b3d072e0ae4094c3476ff8e3b4dbe83dcc
Marcos Aurélio <39933244+marcos-mass13@users.noreply.github.com>
2026-06-24 17:17:04 -0300

    testeb

--- frontend/frontend-aula/src/main.js
+++ frontend/frontend-aula/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import bootstrap from 'bootstrap' 
+import Bootstrap from 'bootstrap' 
 
 /* Bootstrap */
 import 'bootstrap/dist/css/bootstrap.min.css' 
@@ -12,6 +12,6 @@ const app = createApp(App)
 
 app.use(router)
 
-app.use(bootstrap) 
+app.use(Bootstrap) 
 
 app.mount('#app')
