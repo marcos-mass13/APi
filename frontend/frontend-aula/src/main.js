a8d29f55d129673a4b891e8a58c98c64976ada25
Marcos Aurélio <39933244+marcos-mass13@users.noreply.github.com>
2026-06-24 17:13:11 -0300

    teste

--- frontend/frontend-aula/src/main.js
+++ frontend/frontend-aula/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import Bootstrap from 'bootstrap' 
 
 /* Bootstrap */
 import 'bootstrap/dist/css/bootstrap.min.css' 
